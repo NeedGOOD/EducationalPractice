dbc3a6d1ee4979a70b2dd4c791a2a8a029499f8e
_Need <vladyakusevich@gmail.com>
2022-10-27 19:14:04 +0300

    upd: finishing the project

--- PracticalWork#6/task.cpp
+++ PracticalWork#6/task.cpp
@@ -3,73 +3,77 @@
 
 class Train {
 private:
-    std::string station;
+    std::string station;    // Станція
 
-    int numberTrain;
-    int departureTime;
+    int numberTrain;    // Номер вагона
+    // Час відправлення
+    int departureTimeHours;     // Години
+    int departureTimeMinutes;   // Хвилини
 public:
     Train() {
-        std::cout << "station: ";
+        std::cout << "station: ";   // Станція
         std::cin >> station;
 
-        std::cout << "number train: ";
+        std::cout << "number train: ";  // Номер вагона
         std::cin >> numberTrain;
 
-        while (numberTrain < 1) {
+        while (numberTrain < 1) {   // Превірка
             std::cout << "// Error\n";
             std::cout << "Number train: ";
             std::cin >> numberTrain;
         }
-        std::cout << "departure time (hours): ";
-        std::cin >> departureTime;
+        std::cout << "departure time:\n"    // Час відправлення
+                  << "hours: ";             // Години
+        std::cin >> departureTimeHours;
 
-        while (departureTime < 1) {
+        while (departureTimeHours < 0 || departureTimeHours > 23) {     // Превірка
             std::cout << "// Error\n";
-            std::cout << "departure time (hours): ";
-            std::cin >> departureTime;
+            std::cout << "hours: ";
+            std::cin >> departureTimeHours;
+        }
+        std::cout << "minutes: ";           // Хвилини
+        std::cin >> departureTimeMinutes;
+
+        while (departureTimeMinutes < 0 || departureTimeMinutes > 59) { // Превірка
+            std::cout << "// Error\n";
+            std::cout << "minutes: ";
+            std::cin >> departureTimeMinutes;
         }
     }
 
-    std::string GetStation() { return station; }
-    int GetNumberTrain() { return numberTrain; }
-    int GetDepartureTime() { return departureTime; }    
-};
+    static void SortingThree(Train* train, int size) {  // Сортування за пунктом призначення та часом
+        for (int j = 0; j < size; ++j) {    // Сортування
+            int min = j;
 
-void SortingThree(Train* train, int size) {
-    for (int j = 0; j < size; ++j) {
-        int min = j;
-
-        for (int i = j; i < size; ++i) {
-            if (train[min].GetStation() > train[i].GetStation()) { min = i; }
+            for (int i = j; i < size; ++i) {    // Сортування
+                if (train[min].station > train[i].station) { min = i; } // Сортування станцій
+                else if (train[min].station == train[i].station) {
+                    if (train[min].departureTimeHours > train[i].departureTimeHours) { min = i; }   // Сортування за годинами
+                    else if (train[min].departureTimeHours == train[i].departureTimeHours) {
+                        if (train[min].departureTimeMinutes > train[i].departureTimeMinutes) { min = i; }   // Сортування за хвилинами
+                    }
+                }
+            }
+            std::swap(train[min], train[j]);
         }
-        std::swap(train[min], train[j]);
-    }
+        // Вивід
+        std::cout << std::setiosflags(std::ios::left);
+        std::cout << std::setw(15) << "station" << std::setw(15) << "number train"
+                  << std::setw(20) << "departure time" << '\n';
 
-    for (int j = 0; j < size; ++j) {
-        int min = j;
-
-        for (int i = j; i < size; ++i) {
-            if (train[min].GetDepartureTime() > train[i].GetDepartureTime()) { min = i; }
+        for (int i = 0; i < size; ++i) {
+            std::cout << std::setw(15) << train[i].station << std::setw(15) << train[i].numberTrain
+                      << train[i].departureTimeHours << ':' << train[i].departureTimeMinutes << '\n';
         }
-        std::swap(train[min], train[j]);
     }
-    std::cout << std::setiosflags(std::ios::left);
-    std::cout << std::setw(15) << "station" << std::setw(15) << "number train"
-              << std::setw(20) << "departure time" << '\n';
 
-    for (int i = 0; i < size; ++i) {
-        std::cout << std::setw(15) << train[i].GetStation() << std::setw(15) << train[i].GetNumberTrain()
-                  << std::setw(20) << train[i].GetDepartureTime() << '\n';
-    }
-}
-
-void SortingTwo(Train* train, int size) {
+    static void SortingTwo(Train* train, int size) {    // Пошук номер вагона
         int infoTrain;
         
-        std::cout << "enter number train: ";
+        std::cout << "enter number train: ";    // Шукаю вагон
         std::cin >> infoTrain;
 
-        while (infoTrain < 1) {
+        while (infoTrain < 1) {     // Превірка
             std::cout << "// Error\n";
             std::cout << "enter number train: ";
             std::cin >> infoTrain;
@@ -77,38 +81,48 @@ void SortingTwo(Train* train, int size) {
 
         bool haveTrain = false;
 
-        for (int i = 0; i < size; ++i) {
-            if (train[i].GetNumberTrain() == infoTrain) {
-                std::cout << std::setiosflags(std::ios::left);
-                std::cout << std::setw(15) << "station" << std::setw(15) << "number train"
-                          << std::setw(20) << "departure time" << '\n';
-                std::cout << std::setw(15) << train[i].GetStation() << std::setw(15) << train[i].GetNumberTrain()
-                          << std::setw(20) << train[i].GetDepartureTime() << '\n';
+        for (int i = 0; i < size; ++i) {    // Якщо є номер вагона - виводжу
+            if (train[i].numberTrain == infoTrain) {
                 haveTrain = true;
             }
         }
 
-        if (!haveTrain) { std::cout << "Not have this number train.\n"; }
-}
+        if (haveTrain) {    // Якщо є номер вагона - виводжу інформацію
+            std::cout << std::setiosflags(std::ios::left);
+            std::cout << std::setw(15) << "station" << std::setw(15) << "number train"
+                      << std::setw(20) << "departure time" << '\n';
 
-void SortingOne(Train* train, int size) {
-    std::cout << "Before:\n";
-    for (int i = 0; i < size; ++i) { std::cout << train[i].GetNumberTrain() << ' '; }
-    std::cout << '\n';
-
-    for (int j = 0; j < size; ++j) {
-        int min = j;
-        
-        for (int i = j; i < size; ++i) {
-            if (train[min].GetNumberTrain() > train[i].GetNumberTrain()) { min = i; }
+            for (int i = 0; i < size; ++i) {
+                if (train[i].numberTrain == infoTrain) {
+                    std::cout << std::setw(15) << train[i].station << std::setw(15) << train[i].numberTrain
+                              << train[i].departureTimeHours << ':' << train[i].departureTimeMinutes << '\n';
+                }
+            }
         }
-        std::swap(train[min], train[j]);
+
+        // Якщо немає номера вагона - "невиводжу"
+        if (!haveTrain) { std::cout << "Not have this number train.\n"; }
     }
 
-    std::cout << "After:\n";
-    for (int i = 0; i < size; ++i) { std::cout << train[i].GetNumberTrain() << ' '; }
-    std::cout << '\n';
-}
+    static void SortingOne(Train* train,  int size) {   // Сортування номерів вагонів
+        std::cout << "Before:\n";   // Вивід до
+        for (int i = 0; i < size; ++i) { std::cout << train[i].numberTrain << ' '; }
+        std::cout << '\n';
+
+        for (int j = 0; j < size; ++j) {    // Сортування номерів вагонів
+            int min = j;
+            
+            for (int i = j; i < size; ++i) {
+                if (train[min].numberTrain > train[i].numberTrain) { min = i; }
+            }
+            std::swap(train[min], train[j]);
+        }
+
+        std::cout << "After:\n";    // Вивід після
+        for (int i = 0; i < size; ++i) { std::cout << train[i].numberTrain << ' '; }
+        std::cout << '\n';
+    }
+};
 
 int main() {
     const int size = 3;
@@ -117,14 +131,14 @@ int main() {
 
     std::cout << '\n';
 
-    SortingOne(train, size);
+    Train::SortingOne(train, size);
 
     std::cout << '\n';
 
-    SortingTwo(train, size);
+    Train::SortingTwo(train, size);
 
     std::cout << '\n';
 
-    SortingThree(train, size);
+    Train::SortingThree(train, size);
     return 0;
 }
